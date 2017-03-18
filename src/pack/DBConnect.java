0279975139c195c77e48fff8a5972af031f80519
kailiuub <kailiu@buffalo.edu>
2017-03-17 22:23:45 -0400

    finished all types of actions on MySQL DB

--- src/pack/DBConnect.java
+++ src/pack/DBConnect.java
@@ -7,7 +7,7 @@ public class DBConnect {
 	private Statement st;  // query after records are created
 	private ResultSet rs; // hold results
 	private PreparedStatement ps; // action before records are created
-	
+	// Constructor set up link to database
 	public DBConnect() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -18,7 +18,7 @@ public class DBConnect {
 			System.out.println("Error:"+e);
 		}
 	}
-	
+	// Create New Table
 	public void createTable(String tname) {
 		try {
 			String query = "CREATE TABLE IF NOT EXISTS tablename (id int NOT Null AUTO_INCREMENT, first VARCHAR(20) NOT NULL, last VARCHAR(20) NOT NULL, PRIMARY KEY (id))";
@@ -30,7 +30,37 @@ public class DBConnect {
 			System.out.println(e);
 		}
 	}
-	
+	// Copy Table
+	public void copyTable(String newTable, String oldTable) {
+		try {
+			String query1 = "create table newT like oldT";
+			query1 = query1.replace("newT", newTable);
+			query1 = query1.replace("oldT", oldTable);
+			ps = con.prepareStatement(query1);
+			ps.executeUpdate();
+			String query2 = "insert newT select * from oldT";
+			query2 = query2.replace("newT", newTable);
+			query2 = query2.replace("oldT", oldTable);
+			ps = con.prepareStatement(query2);
+			ps.executeUpdate();			
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	// Drop Table
+	public void dropTable(String tname) {
+		try {
+			String query = "DROP TABLE tablename";
+			query = query.replace("tablename", tname);
+			ps = con.prepareStatement(query);
+			ps.executeUpdate();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	// Insert Records
 	public void insertRecord(String tname) {
 		try{
 			String query = "insert into tablename values (null, 'Jaky','Chen'),(null,'Mike','Johnson'),(null, 'Jenny', 'Lopez')";
@@ -42,20 +72,93 @@ public class DBConnect {
 			System.out.println(e);
 		}
 	}
-	
-	public void getData() {
+	// Delete Record
+	public void delRec(String tname, String idn) {
 		try {
-			String query = "select * from newtable";
+			String query = "delete from tablename where id=iden";
+			query = query.replace("tablename", tname);
+			query = query.replace("iden", idn);
+			ps = con.prepareStatement(query);
+			ps.executeUpdate();			
+		}
+		catch (Exception e){
+			System.out.println(e);
+		}
+	}
+	// Set Field Value
+	public void setValue(String tname, String colname, String val, String idn) {
+		try {
+			String query = "UPDATE tablename SET cname=value WHERE id=iden";
+			query = query.replace("tablename", tname);
+			query = query.replace("cname", colname);
+			query = query.replace("value", val);
+			query = query.replace("iden", idn);
+			ps = con.prepareStatement(query);
+			ps.executeUpdate();			
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	// Drop Column
+	public void dropCol(String tname, String cname) {
+		try {
+			String query = "ALTER TABLE tablename DROP colname";
+			query = query.replace("tablename", tname);
+			query = query.replace("colname", cname);
+			ps = con.prepareStatement(query);
+			ps.executeUpdate();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	// Add Column
+	public void addCol(String tname, String cname, String constraint) {
+		try {
+			String query = "ALTER TABLE tablename ADD colname constr";
+			query = query.replace("tablename", tname);
+			query = query.replace("colname", cname);
+			query = query.replace("constr", constraint);
+			ps = con.prepareStatement(query);
+			ps.executeUpdate();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	// Get Records
+	public void getData(String tname) {
+		try {
+			String query = "select * from tablename";
+			query = query.replace("tablename", tname);
 			rs = st.executeQuery(query);
 			System.out.println("Results from DB myjdb");
 			while(rs.next()) {
-				String firstname = rs.getString("first_name");
-				String lastname = rs.getString("last_name");
-				String email = rs.getString("email");
-				System.out.println(firstname+" "+lastname+"--"+email);
+				String iden = rs.getString("id");
+				String firstname = rs.getString("first");
+				String lastname = rs.getString("last");
+				System.out.println(iden+" "+firstname+" "+lastname);
 			}
 		}
 		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (st != null) {
+				st.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		}
+		catch (Exception e) {
 			System.out.println(e);
 		}
 	}
--- src/pack/Main.java
+++ src/pack/Main.java
@@ -3,11 +3,45 @@ package pack;
 public class Main {
 
 	public static void main(String[] args) {
-		DBConnect connect = new DBConnect();
-		connect.getData(); // retrieve data from table existing in DB
-		connect.createTable("table2"); // create a new table in DB
-		connect.insertRecord("table2"); // insert records
+		try {
+			
+			DBConnect connect = new DBConnect();
+			String[] arr = {"table1", "table2", "table3" , "table4"};
+			for (String x : arr) {
+				connect.dropTable(x);
+			}
+			
+			connect.createTable("table1"); // create a new table in DB
+			connect.insertRecord("table1"); // insert records
+			connect.getData("table1");      
+			
+			connect.delRec("table1","3");  // delete record based on id
+			connect.getData("table1");
+			
+			connect.copyTable("table2","table1");  // copy data to another table
+			connect.getData("table2");
+			
+			connect.setValue("table2", "first", "\"Deltagamma\"", "1");  // set value of the field
+			connect.getData("table2");
+			
+			connect.copyTable("table3","table1");  // copy data to another table
+			//connect.dropCol("table3", "last");
+			connect.getData("table3");
+			
+			connect.copyTable("table4","table1");
+			connect.addCol("table4", "middle", "VARCHAR(10) NOT NULL AFTER first");
+			connect.setValue("table4", "middle", "\"P.\"", "1");
+			connect.getData("table4");
+						
+			connect.close();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
 		
+		finally {
+			System.out.println("Running is done");
+		}
 	}
 
 }
